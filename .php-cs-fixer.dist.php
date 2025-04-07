e31c1e0ae7613673b4c3be88dbda0c8ab8f78108
Samuel Lopes <samuelvitorprofissional@gmail.com>
2025-04-06 21:24:21 -0300

    🐛 fix: corrigir instância do PHP-CS-Fixer para compatibilidade com versão atual
    Substitui uso obsoleto de Config::create() por new Config(), conforme mudança na API da ferramenta.
    
    Refs: #6

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,9 +1,11 @@
 <?php
 
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__);
+    ->in(__DIR__)
+    ->exclude('vendor')
+    ->exclude('tools');
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR12' => true,
         'array_syntax' => ['syntax' => 'short'],
